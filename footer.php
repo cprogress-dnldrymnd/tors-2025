a4e260b2a9d312c5b7e58efc6bd65ceef8fd0fc6
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-06-30 07:03:38 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -158,7 +158,7 @@ $query = new WP_Query($args);
             // With pre-decoded audio data
             var $id_val = $id;
             console.log($id_val);
-            const playBTN = document.getElementsByClassName('.play-before-audio-' + $id);
+            const playBTN = document.getElementsByClassName('.play-before-audio-1948');
             const WaveSurfer_TORS = WaveSurfer.create({
                 "container": document.getElementById($id),
                 "height": 50,
