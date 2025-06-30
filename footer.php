e7a41a834e09c6c0496bca0f2d0a29c1777a4292
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-06-30 20:52:31 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -198,12 +198,12 @@ $query = new WP_Query($args);
 
             WaveSurfer_TORS[$id].on('interaction', () => {
                 WaveSurfer_TORS[$id].play();
-                jQuery('#' + $id).parents('.audio-player--player-holder').addClass('playing');
+                jQuery('#' + $id).parents('.audio-player--player').addClass('playing');
             });
 
             WaveSurfer_TORS[$id].on('finish', () => {
                 WaveSurfer_TORS[$id].setTime(0);
-                jQuery('#' + $id).parents('.audio-player--player-holder').removeClass('playing');
+                jQuery('#' + $id).parents('.audio-player--player').removeClass('playing');
 
             });
             WaveSurfer_TORS[$id].on('ready', function() {
