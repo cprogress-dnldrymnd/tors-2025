5ddb1dc51bd38f77012832fe0dfba441ea921f44
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-07-01 20:26:22 +0800

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -399,11 +399,11 @@ function recordings_box($atts)
     echo '<div class="artist-songs--box audio-player--parent before-active audio-player--player ' . $class . '">';
     if ($display_artist) {
 
-
-        $image = carbon_get_term_meta($artist['term_id'], 'image');
+        $artist = get_the_terms(get_the_ID(), 'artists');
 
         echo '<div class="artist-songs--wrapper">';
-        echo do_shortcode('[artist_box image=' . $image . ' name=' . $artist['name'] . ' description=' . $artist['description'] . ']');
+
+        echo do_shortcode('[artist_box term_id=' . $artist[0]->term_id . ']');
     }
     echo '<div class="artist-songs--inner">';
     echo '<div class="artist-songs--title">';
