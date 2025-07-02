fc2f27de73c1a2ea7dd0a5793eac0fae5e77011a
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-07-02 22:45:55 +0800

    cs

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,7 +1,17 @@
 var WaveSurfer_TORS = [];
 jQuery(document).ready(function () {
     console.log('ss')
-    jQuery(".fancybox").fancybox();
+    jQuery(".fancybox").fancybox({
+        maxWidth: 800,
+        maxHeight: 600,
+        fitToView: false,
+        width: '70%',
+        height: '70%',
+        autoSize: false,
+        closeClick: false,
+        openEffect: 'none',
+        closeEffect: 'none'
+    });
 
     checkVisibility();
 
