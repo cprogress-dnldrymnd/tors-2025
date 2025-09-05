264bc204658eb1afb354e087f59429431e7756ec
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-09-05 16:44:36 +0800

    Update main.js

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -71,10 +71,8 @@ jQuery(document).ready(function () {
         e.preventDefault();
     });
 
-    console.log('x4');
     $container_margin = jQuery('.header-area .container').css('margin-left');
-    console.log($container_margin);
-    jQuery('body').attr('style', $container_margin);
+    jQuery('body').attr('style', '--container-margin: ' + $container_margin);
 
 });
 
