e2188e50e4557f43a5e3d6d73e99230719e5ea6e
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-09-05 16:42:17 +0800

    Update main.js

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -71,10 +71,11 @@ jQuery(document).ready(function () {
         e.preventDefault();
     });
 
-    console.log('x3');
+    console.log('x2');
     $container_margin = jQuery('.header-area .container').css('margin-left');
     console.log($container_margin);
-    jQuery('div[data-elementor-type="wp-page"]').css('--container-margin', $container_margin);
+    jQuery('body').css('--container-margin', $container_margin);
+    jQuery('body').css('--container-margin', '11px');
 
 });
 
